8805d4a02a52a308ecc0a18d81688e6298636c16
michalwojcik4 <mwjcik4@gmail.com>
2023-08-05 17:12:42 +0200

    added task 10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,20 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector('#control, input');
+const btnCreate = document.querySelector('#controls button[data-create]');
+const btnDestory = document.querySelector('#controls button[data-destory]');
+const area = document.querySelector('#boxes');
+
+btnCreate.addEventListener('click', createBoxes);
+
+function createBoxes(amount){
+  amount = input.value;
+
+  for(let i = 0; i < amount; i++){
+  const heading = document.createElement("div");
+  
+  area.append(heading)
+  }
+}
